4dc96b5c6822c7ee0d16c6b84894e535ebe6a394
merliariza <180667185+merliariza@users.noreply.github.com>
2025-02-08 22:37:37 -0500

    feat: :sparkles: adding JS features

--- js/app.js
+++ js/app.js
@@ -1,21 +1,75 @@
-function rand(n){
+var imagenes = [
+     "toyota.png",
+     "ford.png",
+     "bmw.jpg",
+     "mazda.png",
+     "suzuki.png",
+     "volkswagen.png",
+     "chevrolet.jpg",
+     "hyundai.png",
+     "honda.jpg",
+     "kia.png"
+   ];
 
-     return(Math.floor(Math.random() * n + 1 ));
-    }
+   var imagenesBarajadas = [];
+   var currentBrand = "";
+   var currentRound = 1;
+   var totalRounds = 10;
+   var correctCount = 0;
+   var incorrectCount = 0;
 
-    var cambia_imagen = new Array();
-    cambia_imagen[0] = "toyota";
-    cambia_imagen[1] = "ford";
-    cambia_imagen[2] = "bmw";
-    cambia_imagen[3] = "mazda";
-    cambia_imagen[4] = "suzuki";
-    cambia_imagen[5] = "volkswagen";
-    cambia_imagen[6] = "chevrolet";
-    cambia_imagen[7] = "hyundai";
-    cambia_imagen[8] = "honda";
-    cambia_imagen[9] = "kia";
-    
+   function shuffle(array) {
+     for (let i = array.length - 1; i > 0; i--){
+       let j = Math.floor(Math.random() * (i + 1));
+       [array[i], array[j]] = [array[j], array[i]];
+     }
+     return array;
+   }
 
-    function cambiar(){
-         document.getElementById("ia").src = cambia_imagen[rand(10)-1];
-    }
+   function cambiar(){
+     var indice = currentRound - 1;
+     var imgName = imagenesBarajadas[indice];
+     document.getElementById("ia").src = "images/" + imgName;
+     currentBrand = imgName.split('.')[0];
+     document.getElementById("brandSelect").value = "";
+   }
+
+   function iniciarJuego(){
+     imagenesBarajadas = shuffle(imagenes.slice());
+     currentRound = 1;
+     correctCount = 0;
+     incorrectCount = 0;
+     document.querySelector(".button_enviar").disabled = false;
+     cambiar();
+   }
+
+   function verificar(){
+     var selectedBrand = document.getElementById("brandSelect").value;
+     if(selectedBrand === ""){
+       alert("Por favor, selecciona una marca.");
+       return;
+     }
+     if(selectedBrand === currentBrand){
+       correctCount++;
+       alert("Respuesta correcta.");
+     } else {
+       incorrectCount++;
+       alert("Respuesta incorrecta. La respuesta correcta era: " + currentBrand);
+     }
+     if(currentRound < totalRounds){
+       currentRound++;
+       cambiar();
+     } else {
+       var mensajeFinal = "";
+       if(correctCount === totalRounds){
+         mensajeFinal = "¡Felicidades!";
+       } else if(correctCount > 7){
+         mensajeFinal = "¡Buen trabajo!";
+       } else {
+         mensajeFinal = "Juego terminado.";
+       }
+       mensajeFinal += "\nRespuestas correctas: " + correctCount + "\nRespuestas incorrectas: " + incorrectCount;
+       alert(mensajeFinal);
+       document.querySelector(".button_enviar").disabled = true;
+     }
+   }
